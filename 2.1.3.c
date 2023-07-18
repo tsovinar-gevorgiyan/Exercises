c06b9ee06004bde546d69425629966b8df2f43e8
tsovinar-gevorgiyan <139646124+tsovinar-gevorgiyan@users.noreply.github.com>
2023-07-18 13:56:25 +0400

    Update 2.1.3.c

--- 2.1.3.c
+++ 2.1.3.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 
 int main()
+
 {
-	
-	int num = 0;
-	int sum = 0;
 
-	printf("Please enter a number: ");
-	scanf("%d", &num);
-	
-	while(num!=0){
+        int num = 0;
+        int sum = 0;
 
-	int balance = num%10;
-	sum += balance;
-	num = (num-balance)/10;
-	}
-		
-	printf("The sum of digits is %d", sum);
-	
+        do{
+        printf("Please enter a number greater than 12: ");
+        scanf("%d", &num);}
+        while(num<=12);
+
+        if(num>12){
+        while(num!=0){
+
+        int balance = num%10;
+        sum += balance;
+        num = (num-balance)/10;
+        }
+
+        printf("The sum of digits is %d", sum);
+        }
+
 }
+                                                                                                                                                                                              
